a1763385573e020905cd9b83ae4ec57d0d6052c4
Chung2vn <Chum910@outlook.com.vn>
2021-11-17 11:52:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/thuchanhui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.thuchanhui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Đây chỉ là UI chứ không đăng nhập được đâu người anh em! :))", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
